c382b66eb97d809c9634996bc03a38ce0ec2869a
Chris Davis <christopher.j.davis03@gmail.com>
2022-06-09 15:13:41 -0500

    HelloWorld

---
+++ src/HelloWorld.java
@@ -0,0 +1,39 @@
+public class HelloWorld {
+    public static void main (String[] args){
+        System.out.println("Hello, new line");
+        System.out.print("Hello, ");
+        System.out.println("World!");
+
+        int myFavoriteNumber = 13;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "Wow, much difficult";
+        System.out.println(myString);
+
+        float myNumber = (float) 3.14;
+        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+        String theNumberThree = "three";
+        Object o = theNumberThree;
+
+        int x = 4;
+        x += 5;
+
+        int a = 3;
+        int b = 4;
+        b *= 3;
+
+        int c = 10;
+        int d = 2;
+
+        c /= d;
+        d -= c;
+
+        byte z = 127;
+        System.out.println(++z);
+    }
+}
