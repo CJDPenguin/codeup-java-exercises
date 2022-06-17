92bae5eab5899a93cca10eaeb70f940eee9e6a10
Chris Davis <christopher.j.davis03@gmail.com>
2022-06-17 09:27:36 -0500

    added count added count

--- src/shapes/CircleApp.java
+++ src/shapes/CircleApp.java
@@ -9,14 +9,17 @@ public class CircleApp {
     public static void main(String[] args) {
             System.out.println("Would you like to make a circle with me, little child?");
             boolean go = in.yesNo();
+            int count = 0;
 
             while(go) {
                 System.out.println("What's the radius of your circle, small one?");
                 double rad = in.getDouble();
                 Circle rosie = new Circle(rad);
                 System.out.printf("Good job, sweetheart! That wasn't too hard was it?\nThe circumference of your circle would be %.2f and the area of your circle would be %.2f\nWe can make another if you'd like?\n", rosie.getCircumference(), rosie.getArea());
+                count++;
                 go = in.yesNo(true);
             }
+        System.out.printf("Amazing! You created %d circles. You did so well youngling.\n", count);
         }
     }
 
