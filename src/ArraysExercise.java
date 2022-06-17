e710fe22079891689c42b4714d5c9151de703a1d
Chris Davis <christopher.j.davis03@gmail.com>
2022-06-17 12:25:04 -0500

    ArraysExercise

---
+++ src/ArraysExercise.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+import static java.util.Arrays.fill;
+
+public class ArraysExercise {
+
+
+    public static String[] people = new String[3];
+
+    public static String[] addPerson (String[] string, String add1) {
+        String[] added = Arrays.copyOf(string, string.length + 1);
+        added [string.length] = add1;
+        return added;
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        System.out.println(numbers);
+
+        fill(people, "Bob");
+
+        String[] check = addPerson(ArraysExercise.people,"Steve");
+
+        for (String person : ArraysExercise.people) {
+            System.out.println(person);
+        }
+        for (String person : check) {
+            System.out.println(person);
+        }
+    }
+
+}
